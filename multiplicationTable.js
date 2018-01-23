8dcb31e0811c49e56931bc83379e44e0fe77cba3
bushblade <bushblade@gmail.com>
2018-01-23 23:42:06 +0000

    improved

--- multiplicationTable.js
+++ multiplicationTable.js
@@ -1,15 +1,13 @@
 //create a function that given a number returns multiplication table up to that number
 
 function createMultiTable(num) {
-  let multiTable = [],
-    row = [];
+  let multiTable = [];
+  let row = [];
   //create first array
   for (let i = 0; i <= num; i++) {
     row.push(i);
   }
-  for (let x = 0; x <= num; x++) {
-    multiTable.push(row.map(item => item * x));
-  }
+  row.forEach((i, x) => multiTable.push(row.map(item => item * x)));
   multiTable.forEach((item, index) => item.splice(item[0], 1, row[index]));
   multiTable[0] = row;
   console.log(multiTable);
