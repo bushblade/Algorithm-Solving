1931ce2ee8a4f7288ced11275c3a8ba7c452af1a
bushblade <bushblade@gmail.com>
2018-01-24 10:17:45 +0000

    No for Loop!

--- multiplicationTable.js
+++ multiplicationTable.js
@@ -2,11 +2,7 @@
 
 function createMultiTable(num) {
   let multiTable = [];
-  let row = [];
-  //create first array
-  for (let i = 0; i <= num; i++) {
-    row.push(i);
-  }
+  let row = new Array(num + 1).fill(0).map((x, i) => x = i);
   row.forEach((i, x) => multiTable.push(row.map(item => item * x)));
   multiTable.forEach((item, index) => item.splice(item[0], 1, row[index]));
   multiTable[0] = row;
