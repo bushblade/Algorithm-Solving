f716b9f9f8d5ea81ffbf81abfad7180e372ae9f8
bushblade <bushblade@gmail.com>
2018-01-31 20:20:54 +0000

    cleaned up

--- countdown.js
+++ countdown.js
@@ -6,12 +6,12 @@ function timer(minutes, seconds = 0) {
     console.log(`time remaining: ${minTwoDidgets(minuteCount)}:${minTwoDidgets(secondsCount)}`);
     totalTime--;
     secondsCount--;
-    secondsCount < 0 ? (secondsCount = 59 , minuteCount --) : false;
+    secondsCount < 0 ? (secondsCount = 59, minuteCount--) : false;
     totalTime < 0 ? clearInterval(interval) : false;
   }, 1000);
 }
 
-function minTwoDidgets(num){
+function minTwoDidgets(num) {
   let x = String(num);
   x.length < 2 ? x = `0${num}` : false;
   return x;
