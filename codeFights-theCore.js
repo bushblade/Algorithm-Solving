e4af51ec2f73e68c534a9aaa122dc2a4daae5ea0
bushblade <bushblade@gmail.com>
2018-03-11 20:20:38 +0000

    reached At the Crossroads

--- codeFights-theCore.js
+++ codeFights-theCore.js
@@ -1,5 +1,61 @@
 //############### intro Gates ################
 
 function addTwoDigits(n) {
-  return [...String(n)].reduce((a,x) => a + Number(x),0)
+  return [...String(n)].reduce((a, x) => a + Number(x), 0)
+}
+console.log(addTwoDigits(29))
+
+
+function largestNumber(n) {
+  return Number('9'.repeat(n))
+}
+console.log(largestNumber(2))
+
+
+function candies(n, m) {
+  return m - m % n
+}
+console.log(candies(3, 10))
+
+function seatsInTheater(nCols, nRows, col, row) {
+  return (nCols - (col - 1)) * (nRows - row)
+}
+console.log(seatsInTheater(16, 11, 5, 3))
+
+function maxMultiple(divisor, bound) {
+  while (bound % divisor !== 0) {
+    bound--
   }
+  return bound
+}
+console.log(maxMultiple(3, 10))
+
+function circleOfNumbers(n, fn) {
+  let result = fn > n / 2 ? fn - n / 2 : fn + n / 2
+  return result === n ? 0 : result
+}
+console.log(circleOfNumbers(6, 3))
+
+function lateRide(n) {
+  return [...`${Math.floor(n/60)}${n % 60}`].reduce((s, x) => s + Number(x), 0)
+}
+console.log(lateRide(808))
+
+function phoneCall(min1, min2_10, min11, s) {
+  return Array(s).fill(0).map((x, i) => {
+    if (i === 0) {
+      return min1
+    } else if (i > 9) {
+      return min11
+    } else {
+      return min2_10
+    }
+  }).reduce((mins, x) => {
+    s = s - x
+    s >= 0 ? mins++ : false
+    return mins
+  }, 0)
+}
+console.log(phoneCall(10, 1, 2, 22))
+
+//############ At the crossroads ###############################
