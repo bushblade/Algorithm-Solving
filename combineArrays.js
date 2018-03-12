3dd2f206557fb981a37f5af841f9d61c8e19f7fc
bushblade <bushblade@gmail.com>
2018-03-12 19:10:01 +0000

    reuced

--- combineArrays.js
+++ combineArrays.js
@@ -1,15 +1,13 @@
-var list1 = [1, 2, 3, 4, 5, 6, 7];
-var list2 = ["a", "b", "c"];
+var list1 = [1, 2, 3, 4, 5, 6, 7]
+var list2 = ["a", "b", "c"]
 //combine the above two arrays so they result as shown in the result array below
-var result = [1, "a", 2, "b", 3, "c", 4, 5, 6, 7];
+var result = [1, "a", 2, "b", 3, "c", 4, 5, 6, 7]
 
 const combine = (list1, list2) => {
-  let newArray = [];
-  list1.forEach((x, i) => {
-    newArray.push(x);
-    list2[i] ? newArray.push(list2[i]) : false;
-  });
-  console.log(newArray);
+  return list1.reduce((a, x, i) => {
+    a.push(x)
+    list2[i] ? a.push(list2[i]) : false
+    return a
+  }, [])
 }
-
-combine(list1, list2);
+console.log(combine(list1, list2))
