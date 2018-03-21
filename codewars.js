6c83c6c9cc713e78b3f88e7cbdd69f1f821d57d5
bushblade <bushblade@gmail.com>
2018-03-21 21:00:10 +0000

    stuck on Maximum subarray

--- codewars.js
+++ codewars.js
@@ -272,11 +272,40 @@ const isValidIP = str => str.split('.').filter(x => !x.match(/\s/) && x >= 0 && 
 // Exclamation marks series #3: Remove all exclamation marks from sentence except at the end
 const remove = s => {
   let check = true,
-      exc = [...s].reverse().reduce((str, x) => {
+    exc = [...s].reverse().reduce((str, x) => {
       if (check) x === '!' ? str += x : check = false
       return str
     }, '')
   return [...s].filter(x => x !== '!').concat(exc).join('')
 }
 
-console.log(remove("Hi!! Hi!!!"))
+// console.log(remove("Hi!! Hi!!!"))
+
+
+const failed = [48, 11, -15, 13, 35, 48, -15, -32, -9, 46, 30, -26, -9, -1, 11, -1, -38, -48, 8, -36, 11, 24, 46, 13, -45, 47, -48, 30, -38, 15, 21, -7, -27, -5, 28, 34, 23, -45, -1, -25, -26, 7, -19, 27, 26, -25, -42, 6, 31, 27, -40, -30, -36]
+
+// Maximum subarray sum
+const maxSequence = function(arr) {
+  let sum = arr.reduce((s, x) => s + x, 0),
+    result = arr
+  const chop = ([head, ...tail]) => {
+    console.log(tail)
+    next = tail
+    let check = tail.reduce((sum, e) => sum += e, 0)
+    if (check > sum) {
+      result = tail
+      sum = check
+      tail.length > 0 ? chop(tail) : false
+    }
+    else {
+      tail.length > 0 ? chop(tail) : false
+    }
+  }
+  chop(arr)
+  chop(result.reverse())
+  console.log(result.reverse())
+  return result.reduce((a, b) => a += b, 0)
+}
+console.log(maxSequence(failed))
+
+
