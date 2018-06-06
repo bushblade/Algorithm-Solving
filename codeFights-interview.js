7c3a34dcc8a1b42043eb4d7bd0bed65d2db46aff
bushblade <bushblade@gmail.com>
2018-06-06 22:30:18 +0100

    testing Atom git panel

--- codeFights-interview.js
+++ codeFights-interview.js
@@ -11,11 +11,55 @@ function firstDuplicate(a) {
   let temp = {}
   for (let i = 0; i < a.length; i++) {
     temp[a[i]] ? temp[a[i]]++ : temp[a[i]] = 1
-      if(temp[a[i]] === 2){
-        return a[i]
-      }
+    if (temp[a[i]] === 2) {
+      return a[i]
     }
+  }
   return -1
 }
 
-console.log(firstDuplicate([2, 3, 3, 1, 5, 2]))
+// console.log(firstDuplicate([2, 3, 3, 1, 5, 2]))
+
+function rotateImage(a) {
+  return a.reverse().map((x, i) => a.map(e => e[i]))
+}
+// console.log(rotateImage([
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9]
+// ]))
+
+function sudoku2(grid) {
+  let solved = false
+  const check = arr => {
+    arr = arr.filter(x => x !== '.')
+    let s = Array.from(new Set([...arr]))
+    s.length === arr.length ? solved = true : solved = false
+  }
+  const cols = grid.map((x, i) => grid.map(e => e[i]))
+  // console.log(cols)
+
+  grid.forEach(a => check(a))
+  cols.forEach(c => check(c))
+  
+  const gridA = grid.splice(0, 3),
+        gridB = grid.splice(0, 3),
+        gridC = grid.splice(0, 3)
+       
+  console.log(gridA)
+  console.log(gridB)
+  console.log(gridB)
+  
+  return solved
+}
+console.log(sudoku2([
+  ['.', '.', '.', '1', '4', '.', '.', '2', '.'],
+  ['.', '.', '6', '.', '.', '.', '.', '.', '.'],
+  ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+  ['.', '.', '1', '.', '.', '.', '.', '.', '.'],
+  ['.', '6', '7', '.', '.', '.', '.', '.', '9'],
+  ['.', '.', '.', '.', '.', '.', '8', '1', '.'],
+  ['.', '3', '.', '.', '.', '.', '.', '.', '6'],
+  ['.', '.', '.', '.', '.', '7', '.', '.', '.'],
+  ['.', '.', '.', '5', '.', '.', '.', '7', '.']
+]))
--- codewars-part2.js
+++ codewars-part2.js
@@ -11,27 +11,47 @@ function solution(input, markers) {
 // console.log(solution("apples, pears # and bananas\ngrapes\nbananas !apples", ["#", "!"]))
 
 // Your order please
-function order(words){
-  return words.split(' ').sort((a,b) => a.match(/\d/g) - b.match(/\d/g)).join(' ') || ''
+function order(words) {
+  return words.split(' ').sort((a, b) => a.match(/\d/g) - b.match(/\d/g)).join(' ') || ''
 }
 
 // console.log(order("is2 Thi1s T4est 3a"))
 
-//GPS navigation
-// complete the function so that it returns the fastest route
-const roads = [
-    {from: 0, to: 1, drivingTime: 5},
-    {from: 0, to: 2, drivingTime: 10},
-    {from: 1, to: 2, drivingTime: 10},
-    {from: 1, to: 3, drivingTime: 2},
-    {from: 2, to: 3, drivingTime: 2},
-    {from: 2, to: 4, drivingTime: 5},
-    {from: 3, to: 2, drivingTime: 2},
-    {from: 3, to: 4, drivingTime: 10}
-]
-
-function navigate(numberOfIntersections, roads, start, finish) {
-  return roads.map(x => Object.entries(x))
+String.prototype.toAlternatingCase = function () {
+  return [...this].reduce((str, ltr) => /[a-z]/.test(ltr) ? str += ltr.toUpperCase() : str += ltr.toLowerCase(), '')
 }
-console.log(navigate(5, roads, 0, 4))
-// should return [0, 1, 3, 2, 4]. Fastest time is 5 + 2 + 2 + 5 = 14 minutes
+
+// console.log('TesTing'.toAlternatingCase())
+function validatePIN(pin) {
+  return [...pin].every(l => /\d/.test(l)) && (pin.length === 4 || pin.length === 6)
+}
+
+// console.log(validatePIN('1234'))
+
+function highestRank(arr) {
+  const hash = arr.reduce((obj, n) => {
+    obj[n] ? obj[n]++ : obj[n] = 1
+    return obj
+  }, {})
+  const most = hash[Object.keys(hash).sort((a, b) => hash[b] - hash[a])[0]]
+  return Number(Object.keys(hash).filter((key) => hash[key] === most).sort((a, b) => b - a)[0])
+}
+
+// console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12]))
+
+var countSheep = function (num) {
+  return Array(num).fill().map((x, i) => `${i+1} sheep...`).join('')
+}
+
+// console.log(countSheep(3))
+
+function solution(number) {
+  return [...Array(number - 1)].map((x, i) => i + 1).reduce((arr, x) => {
+    if (x % 5 === 0 && x % 3 === 0) arr[2]++
+      if (x % 5 === 0 && x % 3 !== 0) arr[1]++
+        if (x % 3 === 0 && x % 5 !== 0) arr[0]++
+          return arr
+  }, [0, 0, 0])
+}
+
+console.log(solution(20))
