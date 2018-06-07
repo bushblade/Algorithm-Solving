b8cb8adc0b6868fd6a6965fea543316c0d1ff72a
bushblade <bushblade@gmail.com>
2018-06-07 19:20:07 +0100

    a few easy ones to keep up with it

--- codewars-part2.js
+++ codewars-part2.js
@@ -17,7 +17,7 @@ function order(words) {
 
 // console.log(order("is2 Thi1s T4est 3a"))
 
-String.prototype.toAlternatingCase = function () {
+String.prototype.toAlternatingCase = function() {
   return [...this].reduce((str, ltr) => /[a-z]/.test(ltr) ? str += ltr.toUpperCase() : str += ltr.toLowerCase(), '')
 }
 
@@ -39,7 +39,7 @@ function highestRank(arr) {
 
 // console.log(highestRank([12, 10, 8, 12, 7, 6, 4, 10, 12]))
 
-var countSheep = function (num) {
+var countSheep = function(num) {
   return Array(num).fill().map((x, i) => `${i+1} sheep...`).join('')
 }
 
@@ -54,4 +54,42 @@ function solution(number) {
   }, [0, 0, 0])
 }
 
-console.log(solution(20))
+// console.log(solution(20))
+
+function solve(a, b) {
+  let alice = 0,
+    bob = 0
+  a.forEach((x, i) => {
+    if (x > b[i]) alice++
+      else if (x < b[i]) bob++
+  })
+  if (alice > bob) {
+    return `${alice}, ${bob}: Alice made "Kurt" proud!`
+  } else if (bob > alice) {
+    return `${alice}, ${bob}: Bob made "Jeff" proud!`
+  } else {
+    return `${alice}, ${bob}: that looks like a "draw"! Rock on!`
+  }
+}
+
+// console.log(solve([47, 67, 22], [26, 47, 12]))
+
+function balancedNum(number) {
+  const str = String(number)
+  let [left, right] = [...str].reduce((arr, num, i, a) =>{
+    if (i < a.length / 2) arr[0].push(num)
+    else arr[1].push(num)
+    return arr
+  }, [[],[]])
+  if(str.length % 2 ===0){
+    left.pop()
+    right.shift()
+  } else{
+    left.pop()
+  }
+  left = left.reduce((sum, num) => sum += Number(num),0)
+  right = right.reduce((sum, num) => sum += Number(num),0)
+  return left === right ? "Balanced" : "Not Balanced"
+}
+
+console.log(balancedNum(29559))
