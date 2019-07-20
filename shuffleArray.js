af9af1b40501fabb8278db1cef09be1b00235f74
Will Adams <bushblade@gmail.com>
2019-07-20 20:39:36 +0100

    use Array.reduce

--- shuffleArray.js
+++ shuffleArray.js
@@ -1,27 +1,41 @@
-const arrayToShuffle1 = ["a", "b", "c", "d", "e", "f", "g"];
-const arrayToShuffle2 = ["a", "b", "c", "d", "e", "f", "g"];
+const arrayToShuffle1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+const arrayToShuffle2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
 
 function shuffleArray(array) {
-   for (let i = array.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = array[i];
-      array[i] = array[j];
-      array[j] = temp;
-   }
-   return array;
+  for (let i = array.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1))
+    var temp = array[i]
+    array[i] = array[j]
+    array[j] = temp
+  }
+  return array
 }
 
-console.log(shuffleArray(arrayToShuffle1));
+// console.log(shuffleArray(arrayToShuffle1));
 
 //without a loop
 
-const shuffle = arry => {
-   arry.forEach((x, i) => {
-      let rnd = Math.floor(Math.random() * arry.length),
-         temp = arry[i]
-      arry[i] = arry[rnd]
-      arry[rnd] = temp
-   })
-  return arry
-}
-shuffle(arrayToShuffle2)
+// const shuffle = arry => {
+//   const shuffled = [...arry]
+//   shuffled.forEach((x, i) => {
+//     const rnd = Math.floor(Math.random() * shuffled.length),
+//       temp = shuffled[i]
+//     shuffled[i] = shuffled[rnd]
+//     shuffled[rnd] = temp
+//   })
+//   return shuffled
+// }
+
+const shuffle = arry =>
+  arry.reduce(
+    (acc, _, i) => {
+      const rnd = Math.floor(Math.random() * acc.length),
+        temp = acc[i]
+      acc[i] = acc[rnd]
+      acc[rnd] = temp
+      return acc
+    },
+    [...arry]
+  )
+
+console.log(shuffle(arrayToShuffle2))
