5abd6eb8508dcc8bf34927bafcc31f4977df792a
Will Adams <bushblade@gmail.com>
2019-08-13 14:25:04 +0100

    description

--- dailyInterviewPro.js
+++ dailyInterviewPro.js
@@ -16,6 +16,14 @@ const addLinkedLists = (list1, list2) => {
 
 // console.log(addLinkedLists([2, 4, 3], [5, 6, 4]))
 
+// Imagine you are building a compiler. Before running any code, the compiler must check that the parentheses in the program are balanced. Every opening bracket must have a corresponding closing bracket. We can approximate this using strings.
+
+// Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+// An input string is valid if:
+// - Open brackets are closed by the same type of brackets.
+// - Open brackets are closed in the correct order.
+// - Note that an empty string is also considered valid.
+
 const validBrackets = str => {
   if (str.length % 2 !== 0) return false
   return str.length === 0 ? true : validBrackets(str.replace(/(\(\))|(\[\])|(\{\})/gi, ''))
